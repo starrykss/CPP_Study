6014ba6f8d1448c3dc1b9a8312f86c1a70e22fe1
Sangsoon Kim <kss34891@gmail.com>
2018-01-31 13:36:03 +0900

    Add files via upload

---
+++ Baekjoon/과자.cpp
@@ -0,0 +1,21 @@
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int K, N, M, price;   // K : 과자 한 개의 가격, N : 과자의 개수, M : 현재 가진 돈의 액수
+	
+	cin >> K >> N >> M;
+	price = K * N;
+
+	if (price <= M) {		// 과자 값보다 현재 가진 돈이 더 많을 때
+		cout << "0" << endl;
+	}
+	else {						// 과자 값보다 현재 가진 돈이 더 적을 때
+		cout << price - M << endl;
+	}
+
+	return 0;
+}
+*/
